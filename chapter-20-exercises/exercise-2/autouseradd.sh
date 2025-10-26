a3915c317cfa8cd510ec268c781acc7c3b9ccd42
Miguel Pérez Colino <miguel.perez@suse.com>
2025-10-26 11:03:28 +0100

    Updated script for ch20 ex2

--- chapter-20-exercises/exercise-2/autouseradd.sh
+++ chapter-20-exercises/exercise-2/autouseradd.sh
@@ -2,10 +2,9 @@
 cat users.txt| while read -r line ; do
 NEWUSERNAME=$(echo ${line}|cut -d ";" -f 1|xargs)
 NEWUID=$(echo ${line}|cut -d ";" -f 3|xargs)
-NEWGID=$(echo ${line}|cut -d ";" -f 4|xargs)
 NEWNAME=$(echo ${line}|cut -d ";" -f 5|xargs)
 NEWHOME=$(echo ${line}|cut -d ";" -f 6|xargs)
 NEWSHELL=$(echo ${line}|cut -d ";" -f 7|xargs)
-useradd -d "${NEWHOME}" -m -s "${NEWSHELL}" -u "${NEWUID}" -g "${NEWGID}" -c "${NEWNAME}" "${NEWUSERNAME}"
+useradd -d "${NEWHOME}" -m -s "${NEWSHELL}" -u "${NEWUID}" -U -c "${NEWNAME}" "${NEWUSERNAME}"
 done
 
