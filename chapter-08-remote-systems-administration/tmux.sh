5f20d03d0009735ddc91e514d89e4dd5376027e0
Thorsten Kukuk <kukuk@suse.com>
2025-10-31 21:06:51 +0100

    Convert shell scripts from UTF-8 to ASCII
    The shell scripts don't work correct with UTF-8 characters, bash
    and tmux expect ASCII.

--- chapter-08-remote-systems-administration/tmux.sh
+++ chapter-08-remote-systems-administration/tmux.sh
@@ -5,14 +5,14 @@ SESSION=$USER
 tmux -2 new-session -d -s $SESSION # create new session 
 tmux select-window -t $SESSION:0  # select first window 
 tmux rename-window -t $SESSION "monitoring" #rename to monitoring 
-tmux split-window –h #split horizontally 
-tmux split-window –v #split vertically 
-tmux split-window –h # split again horizontally 
+tmux split-window -h #split horizontally 
+tmux split-window -v #split vertically 
+tmux split-window -h # split again horizontally 
 tmux select-layout tiled #tile panes 
-tmux selectp –t1 # select pane 1 
+tmux selectp -t1 # select pane 1 
 tmux send-keys "top" C-m #run top by sending the letters + RETURN 
-tmux selectp –t2 # select pane 2 
+tmux selectp -t2 # select pane 2 
 tmux send-keys "journalctl -f" C-m # run journalctl 
-tmux selectp –t3 # select pane 3 
+tmux selectp -t3 # select pane 3 
 tmux send-keys "iostat -x" C-m # run iostat 
-tmux selectp –t0 #select the pane without commands executed 
+tmux selectp -t0 #select the pane without commands executed 
